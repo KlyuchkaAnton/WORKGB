bc1c1461e1d213f0151eb942b4ac3f42e9d949f6
antonklyuchka <289743@gmail.com>
2022-11-14 15:41:23 +0300

    Квадрат 2х чисел Вариант 1

--- SEMI1/Program.cs
+++ SEMI1/Program.cs
@@ -10,3 +10,15 @@
 // 111
 // 11
 // 1
+
+//Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя)
+
+// Вариант 1
+
+// Console.WriteLine("Ведите чило: ");
+// int number = int.Parse(Console.ReadLine()!);
+// int sqr = number * number;
+// Console.WriteLine($"Квадрат числа {number} = {sqr}");
+
+
+
