9a6b36f5f8143ccaf4ceb47bc3d0b2b5e8d60899
antonklyuchka <289743@gmail.com>
2022-11-14 16:58:07 +0300

    Задание: Принять на вход 2 числа и проверить является ли первое чиcло квадратом второго

--- SEMI1/Program.cs
+++ SEMI1/Program.cs
@@ -15,21 +15,23 @@
 
 // Вариант 1
 
-// Console.WriteLine("Ведите чило: ");
+// Console.WriteLine("Ведите число: ");
 // int number = int.Parse(Console.ReadLine()!);
 // int sqr = number * number;
 // Console.WriteLine($"Квадрат числа {number} = {sqr}");
 
 // Вариант 2
 
-// Console.WriteLine("Ведите чило: ");
+// Console.WriteLine("Ведите число: ");
 // int number = int.Parse(Console.ReadLine()!);
 // int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
 // Console.WriteLine($"Квадрат числа {number} = {sqr1}");
 
+
 // Задание: Вывести все целые числа от -N до N
 
-// Console.WriteLine("Ведите чило: ");
+
+// Console.WriteLine("Ведите число: ");
 // int N = int.Parse(Console.ReadLine()!);
 // int N2 = - N;
 
@@ -38,3 +40,28 @@
 //     Console.Write($"{N2} ");
 //     N2++;
 // }
+
+
+// Задание: Принять на вход 3х значное число и на выходе показать последний знак этого числа.
+
+// Console.WriteLine("Ведите 3х значное число: ");
+// int N = int.Parse(Console.ReadLine()!);
+// int S = N%10;
+// Console.Write($"Третье {S} ");
+
+// Задание: Принять на вход 2 числа и проверить является ли первое чиcло квадратом второго.
+
+Console.WriteLine("Ведите число A: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Ведите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+if (A==B/A)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+// Задание: Выдать названия дней недели по заданному номеру
