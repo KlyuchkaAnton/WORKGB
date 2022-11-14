27716985e45e422a028436be0b2263d6baa02965
antonklyuchka <289743@gmail.com>
2022-11-14 16:14:48 +0300

    Вывели  число от -N до N

--- SEMI1/Program.cs
+++ SEMI1/Program.cs
@@ -20,5 +20,21 @@
 // int sqr = number * number;
 // Console.WriteLine($"Квадрат числа {number} = {sqr}");
 
+// Вариант 2
 
+// Console.WriteLine("Ведите чило: ");
+// int number = int.Parse(Console.ReadLine()!);
+// int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+// Console.WriteLine($"Квадрат числа {number} = {sqr1}");
 
+// Задание: Вывести все целые числа от -N до N
+
+// Console.WriteLine("Ведите чило: ");
+// int N = int.Parse(Console.ReadLine()!);
+// int N2 = - N;
+
+// while (N2 <= N)
+// {
+//     Console.Write($"{N2} ");
+//     N2++;
+// }
