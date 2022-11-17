b661e5e3d91ebe675b721b239202588326b04547
antonklyuchka <289743@gmail.com>
2022-11-17 20:16:47 +0300

    Разобрачи пример с выводом 2х символов из 3х значного числа

--- SEMI_C#_2/Program.cs
+++ SEMI_C#_2/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("111111111Hello, World!");
+﻿///////// 
+/////  Задача S2_001  -  Написать программу которая выводит случайное 3х значное число и удаляет 2ю цифру этого числа.
+///////// 
 
+// 456 -->  46
+
+// Первый метод математический (1й вариант)
+/*
+int number = new Random().Next(100, 1000); // последнее число которое будет взято 999
+Console.WriteLine(number);
+Console.WriteLine($"Число без второй цифры имеет вид: {number/100}{number%10}");
+*/
+
+// Первый метод математический (2й вариант)
+/*
+int number = new Random().Next(100, 1000); // последнее число которое будет взято 999
+int result = (number/100) * 10 + number%10;
+Console.WriteLine(number);
+Console.WriteLine(result);
+*/
+
+// Второй метод строковый
+/*
+
+Console.WriteLine ("Введи 3х значное число: ");
+string Numbers = Console.ReadLine()!;
+
+if (Numbers.Length != 3 )
+{
+    Console.WriteLine("Введено неверное число");
+}
+else
+{
+    Console.WriteLine($"Число без второй цифры имееет вид: {Numbers[0]}{Numbers[2]}");
+}
+// строка от 1 до бес, ;№;:*, ЛДПФЦКПРФКМТ 
+// массив только цифры от 1 до беск.
+*/
