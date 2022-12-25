cd5b8588639932c22ce18b40ebf5e691ff7114a4
Anton <115809418+KlyuchkaAnton@users.noreply.github.com>
2022-12-25 03:44:44 +0300

    1

---
+++ example2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Ведите число от 1 до 7");
+int P = int.Parse(Console.ReadLine()!);
+
+if (P==1) Console.WriteLine("Понедельник ");
+if (P==2) Console.WriteLine("Вторник ");
+if (P==3) Console.WriteLine("Среда ");
+if (P==4) Console.WriteLine("Четверг ");
+if (P==5) Console.WriteLine("Пятница ");
+if (P==6) Console.WriteLine("Суббота ");
+if (P==7) Console.WriteLine("Воскресенье ");
